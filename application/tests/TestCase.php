8f742a8a2658b4682f89033213fdd6955f05ed36
Nicolas <nicolas.martins@phonetrack.com.br>
2026-05-20 22:42:22 -0300

    tests: rabbit mock ajust

--- application/tests/TestCase.php
+++ application/tests/TestCase.php
@@ -5,7 +5,6 @@ namespace Tests;
 use App\Infrastructure\Queue\RabbitMQ;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
-use Illuminate\Support\Facades\Http;
 
 abstract class TestCase extends BaseTestCase
 {
@@ -13,9 +12,9 @@ abstract class TestCase extends BaseTestCase
 
     protected ?string $authToken = null;
 
-    protected function setUp(): void
+    protected function refreshApplication(): void
     {
-        parent::setUp();
-        $this->mock(RabbitMQ::class);
+        parent::refreshApplication();
+        $this->app->singleton(RabbitMQ::class, fn () => \Mockery::mock(RabbitMQ::class)->shouldIgnoreMissing());
     }
 }
